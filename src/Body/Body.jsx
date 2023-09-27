36c173de1e93f2a34a69481231a82fbee4675df2
itisameerkhan <itisameerkhan@gmail.com>
2023-09-27 09:40:19 +0530

    27/09/2023 09:40

--- src/Body/Body.jsx
+++ src/Body/Body.jsx
@@ -10,8 +10,8 @@ const Body = () => {
 
     useEffect(()=> {
         fetchData();
-    },[])
-
+    },[]);
+    
     const fetchData = async () => {
         const data = await fetch('https://www.swiggy.com/dapi/restaurants/list/v5?lat=13.0826802&lng=80.2707184&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING');
         const json = await data.json();
